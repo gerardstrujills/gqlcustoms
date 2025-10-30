e9230b47d1776baf43518b84ad438b34d5704375
supabasecore <145882347+supabasecore@users.noreply.github.com>
2025-10-29 22:12:51 -0500

    add commit fix querys

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ const main = async () => {
   );
 
   const apolloServer = new ApolloServer({
-    persistedQueries: false,,
+    persistedQueries: false,
     schema: await buildSchema({
       resolvers: [
         UserResolver,
